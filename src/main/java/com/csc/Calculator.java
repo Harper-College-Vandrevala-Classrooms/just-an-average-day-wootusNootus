4b2bd27fc0e89b8629ca153575bd71271370a7a9
wootusNootus <migospodinoff@gmail.com>
2024-12-08 20:09:30 -0600

    Completed

--- src/main/java/com/csc/Calculator.java
+++ src/main/java/com/csc/Calculator.java
@@ -1,7 +1,56 @@
 package com.csc;
 
+import java.util.Arrays;
+
 public class Calculator {
-  public static void main(String[] args) {
-    System.out.println("This class can house your driver program and / or your functions.");
+  public int findMax(int[] nums){
+    int max = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      if(nums[i] > max){
+        max = nums[i];
+      }
+    }
+
+    return max;
+  }
+
+  public int findMin(int[] nums){
+    int min = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      if(nums[i] < min){
+        min = nums[i];
+      }
+    }
+
+    return min;
+  }
+
+  public int sum(int[] nums){
+    int sum = 0;
+    for(int i = 0; i < nums.length; i++){
+      sum += nums[i];
+    }
+
+    return sum;
+  }
+
+  public int average(int[] nums){
+    return sum(nums) / nums.length;
+  }
+
+  public int findMaxEZ(int[] nums){
+    return Arrays.stream(nums).max().getAsInt();
+  }
+
+  public int findMinEZ(int[] nums){
+    return Arrays.stream(nums).min().getAsInt();
+  }
+
+  public double averageEZ(int[] nums){
+    return Arrays.stream(nums).average().getAsDouble();
+  }
+
+  public int sumEZ(int[] nums){
+    return Arrays.stream(nums).sum();
   }
 }
--- src/test/java/com/csc/TestCalculator.java
+++ src/test/java/com/csc/TestCalculator.java
@@ -15,7 +15,50 @@ public class TestCalculator {
   }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void testFindMax() {
+    int[] nums = {1, 3, 7, 2, 5};
+    assertEquals(7, calculator.findMax(nums));
   }
+
+  @Test
+  void testFindMin() {
+    int[] nums = {1, 3, 7, 2, 5};
+    assertEquals(1, calculator.findMin(nums));
+  }
+
+  @Test
+  void testSum() {
+    int[] nums = {1, 3, 7, 2, 5};
+    assertEquals(18, calculator.sum(nums));
+  }
+
+  @Test
+  void testAverage() {
+    int[] nums = {1, 3, 7, 2, 5};
+    assertEquals(3, calculator.average(nums)); // Integer division
+  }
+
+  @Test
+    void testFindMaxEZ() {
+        int[] nums = {1, 3, 7, 2, 5};
+        assertEquals(7, calculator.findMaxEZ(nums));
+    }
+
+    @Test
+    void testFindMinEZ() {
+        int[] nums = {1, 3, 7, 2, 5};
+        assertEquals(1, calculator.findMinEZ(nums));
+    }
+
+    @Test
+    void testSumEZ() {
+        int[] nums = {1, 3, 7, 2, 5};
+        assertEquals(18, calculator.sumEZ(nums));
+    }
+
+    @Test
+    void testAverageEZ() {
+        int[] nums = {1, 3, 7, 2, 5};
+        assertEquals(3.6, calculator.averageEZ(nums), 0.0001); // Double precision comparison
+    }
 }
